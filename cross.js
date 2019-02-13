ac2a05fb5278e92baaf1aee09be7ff5e4b09d822
tony <tony@linktime.cloud>
2019-02-13 10:57:37 +0800

    判断两条线段是否相交

---
+++ cross.js
@@ -0,0 +1,24 @@
+/**
+ * 判断两条线段是否相交
+ * eg: [[1,0],[0,1]], [[2,1],[0,0]]
+ */
+
+function isCross ([m1, m2], [n1, n2]) {
+    try {
+        // line1
+        let k1 = (m2[1] - m1[1]) / (m2[0] - m1[0])
+        let b1 = (m1[1] - k1 * m1[0]) 
+        // line2
+        let k2 = (n2[1] - n1[1]) / (n2[0] - n1[0])
+        let b2 = (n1[1] - k2 * n1[0])
+        // 求交点
+        let x0 = (b2 - b1) / (k1 - k2)
+        if ((x0 < Math.max(m1[0], m2[0])) && (x0 > Math.min(m1[0], m2[0])) 
+            && (x0 < Math.max(n1[0], n2[0])) && (x0 > Math.min(n1[0], n2[0]))) {
+            return true
+        }
+        return false
+    } catch (e) {
+        console.error('数据格式有误')
+    }
+}
